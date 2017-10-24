f977b39941afb709c67838d95fad8ba2d5f0c37f
Vinko Kolobara <kolobara.vinko@gmail.com>
2017-10-24 20:10:06 +0200

    SVM classifier mapper added

---
+++ lab/lab1/ksvm_wrap.py
@@ -0,0 +1,47 @@
+from sklearn import svm
+import numpy as np
+from lab.lab1.data import *
+
+class KSVMWrap(object):
+
+    def __init__(self, X, Y_, param_svm_c=1, param_svm_gamma='auto'):
+        self.X = X
+        self.Y_ = Y_
+        self.model = svm.SVC(C=param_svm_c, gamma=param_svm_gamma, probability=True)
+        self.model.fit(X, Y_)
+        self.support = self.model.support_
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def get_scores(self, X):
+        return self.model.predict_proba(X)
+
+
+if __name__ == "__main__":
+    # inicijaliziraj generatore slučajnih brojeva
+    np.random.seed(100)
+
+    # instanciraj podatke X i labele Yoh_
+    #X, Y_ = sample_gauss(2, 100)
+    X, Y_ = sample_gmm(6, 2, 10)
+
+    ksvm = KSVMWrap(X, Y_)
+
+    # nauči parametre:
+    # dohvati vjerojatnosti na skupu za učenje
+    probs = ksvm.get_scores(X)
+    Y = ksvm.predict(X)
+
+
+    # ispiši performansu (preciznost i odziv po razredima)
+    accuracy, pr, M = eval_perf_multi(Y, Y_)
+    print(accuracy, pr)
+
+    # iscrtaj rezultate, decizijsku plohu
+    rect = (np.min(X, axis=0), np.max(X, axis=0))
+
+    graph_surface(lambda x: ksvm.predict(x), rect, offset=0)
+    graph_data(X, Y_, Y, special=ksvm.support)
+
+    plt.show()
